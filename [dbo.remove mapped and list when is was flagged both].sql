02c7425e756d064b87bec39f1a058715c258fe56
Eyes Well <chenerjun@gmail.com>
2020-01-23 14:48:17 -0500

    Delete [dbo.remove mapped and list when is was flagged both].sql

--- [dbo.remove mapped and list when is was flagged both].sql
+++
@@ -1,5 +0,0 @@
-MERGE INTO RAMResource AS T
-using (select  resourceagencynum  from RamResource where map= 'BOTH' ) as S
-on (T.resourceagencynum = S.resourceagencynum   and T.map <> 'BOTH')
-WHEN   MATCHED THEN 
-Delete;
